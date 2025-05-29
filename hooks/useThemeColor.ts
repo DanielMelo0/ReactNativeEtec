d2718f275033479d39f5cea02976361048a3361b
Daniel Melo <79672542+DanielMelo0@users.noreply.github.com>
2025-05-29 17:29:13 -0300

    Update useThemeColor.ts

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -13,7 +13,7 @@ export function useThemeColor(
   const theme = useColorScheme() ?? 'light';
   const colorFromProps = props[theme];
 
-  if (colorFromProps) {
+  if (colorFromProps) { 
     return colorFromProps;
   } else {
     return Colors[theme][colorName];
